8498b97c1183741991145d38dc8f9d5fcf6a5c87
Clara <Clara@general>
2015-12-09 11:54:51 +0100

    main creado

---
+++ ProyectoEgit/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) { 
+	// TODOAuto-generated method stub
+	  Scanner sc = new Scanner(System.in); 
+	  int numero1, numero2, resultado; 
+	  System.out.print("Introduce primer número: "); 
+	  numero1 = sc.nextInt(); 
+	  System.out.print("Introduce segundo número: "); 
+	  numero2 = sc.nextInt(); 
+	 } 
+
+
+}
